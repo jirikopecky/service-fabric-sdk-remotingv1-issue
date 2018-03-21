29c65003ab29e2f4fa2f6e85351d0bbf22633b4c
Jiri Kopecky <jiri.kopecky@honeywell.com>
2018-03-21 14:21:16 +0100

    Added minimal repro for Service Fabric reference issue.

---
+++ SFReferenceIssueRepro/HeaderProcessorBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SFReferenceIssueRepro
+{
+    public abstract class HeaderProcessorBase
+    {
+        public HeaderProcessorBase NextProcessor { get; private set; }
+
+        protected abstract void Process(ServiceRemotingMessageHeaders headers);
+
+        public void SetNextProcessor(HeaderProcessorBase nextProcessor)
+        {
+            NextProcessor = nextProcessor;
+        }
+
+        public void Handle(ServiceRemotingMessageHeaders headers)
+        {
+            Process(headers);
+            NextProcessor?.Handle(headers);
+        }
+    }
+}
